1c3e806b122ed83017c6cbeb2dd2fea77289ecad
Mireille Raad <mir.mir@gmail.com>
2014-05-29 01:39:57 +0300

    adjusting counter

--- php/leftData.php
+++ php/leftData.php
@@ -3,7 +3,7 @@
 
 	require_once ("dbog.php");
 
-	$i=0; $q=mysql_query('select chart4b, chart4c from results order by countryname');
+	$i=0; $q=mysql_query('select chart3a, chart3b from results order by countryname');
 	 
 	if ( ! $q ) {
 	echo mysql_error();
@@ -13,7 +13,7 @@
  $data =  ""; 
   while($row=mysql_fetch_array($q)){
  
-  $data= $data .  intval($row['chart4b'] + $row['chart4c'])  . "," ;
+  $data= $data .  intval($row['chart3a']) . "," .  intval($row['chart3b'])  . "," ;
 
       $i++;
 
--- php/rightData.php
+++ php/rightData.php
@@ -3,7 +3,7 @@
 
 	require_once ("dbog.php");
 
-	$i=0; $q=mysql_query('select chart4a from results order by countryname');
+	$i=0; $q=mysql_query('select chart3c, chart3d from results order by countryname');
 	 
 	if ( ! $q ) {
 	echo mysql_error();
@@ -13,7 +13,8 @@
  $data =  ""; 
   while($row=mysql_fetch_array($q)){
  
-  $data= $data .  intval($row['chart4a']) . "," ;
+  
+   $data= $data .  intval($row['chart3c']) . "," .  intval($row['chart3d'])  . "," ;
 
       $i++;
 
